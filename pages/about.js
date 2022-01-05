9ed1e2d64f10adc540dcc2dedc3b5203c68c66d8
shn <topdog.shn.01@gmail.com>
2022-01-05 13:41:17 +0900

    [add]about.js

--- pages/about.js
+++ pages/about.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Index Page
+          About Page
         </h1>
 
         <p className={styles.description}>
