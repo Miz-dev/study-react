ca68903c6ccbefb753732626d71d7f1d2a41dc43
shn <topdog.shn.01@gmail.com>
2022-01-08 21:12:27 +0900

    💄 スタイルファイルの分割

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
 	return (
