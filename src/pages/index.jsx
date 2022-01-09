79f51ba62fbbcf9cd02cc3cbd4bd110c12588e1c
shn <topdog.shn.01@gmail.com>
2022-01-09 12:34:30 +0900

    ⚗️ 試験的にクリックイベントを記述（後で削除予定）

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
@@ -6,12 +6,23 @@ import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
 export default function Home() {
+	const foo = 1;
+
+	const handleClick = useCallback((e) => {
+		console.log(e.target.href);
+		e.preventDefault();
+		alert(foo);
+	}, []);
+
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Index Page</title>
 			</Head>
 			<Header />
+			<a href="/about" onClick={handleClick}>
+				ボタン
+			</a>
 			<Main page="index" />
 			<Footer />
 		</div>
