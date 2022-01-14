cb0e8fd9a6a3a87d943658b40198a77a5010aad6
shn <topdog.shn.01@gmail.com>
2022-01-14 10:25:38 +0900

    ✨ useMemoをcountに適用

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,7 +6,7 @@ import { Main } from "src/components/Main";
 import { Footer } from "src/components/Footer";
 
 export default function About({
-	count,
+	doubleCount,
 	isShow,
 	handleClick,
 	handleDisplay,
@@ -24,7 +24,7 @@ export default function About({
 
 			<button onClick={handleClick}>ボタン</button>
 			<button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-			{isShow ? <h1>{count}</h1> : null}
+			{isShow ? <h2>{doubleCount}</h2> : null}
 
 			<input type="text" value={text} onChange={handleChange} />
 			<button onClick={handleAdd}>追加</button>
