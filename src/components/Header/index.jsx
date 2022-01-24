c3ae3764d2c3594bc6feaa4540a6b03c512a6824
shn <topdog.shn.01@gmail.com>
2022-01-25 08:34:27 +0900

    🚚 投稿系はpostsにパスを変更

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import classes from "src/components/Header/Header.module.css";
 
 const NAV_ITEMS = [
-	{ href: "/", label: "Index" },
+	{ href: "/posts", label: "Posts" },
 	{ href: "/about", label: "About" },
 ];
 
