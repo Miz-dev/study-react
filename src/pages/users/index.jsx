8c7c9266831209505e775e2bc11ed8f90c5d0fc6
shn <topdog.shn.01@gmail.com>
2022-01-30 19:30:03 +0900

    ✨ SWRをImmutable化

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -2,15 +2,34 @@ import React from "react";
 import Head from "next/head";
 import { Header } from "src/components/Header";
 import { UsersComponent } from "src/components/Users";
+import { SWRConfig } from "swr";
 
-const Users = () => {
+export const getServerSideProps = async () => {
+	// ユーザー情報の取得
+	const USERS_API_URL = `https://jsonplaceholder.typicode.com/users`;
+	const users = await fetch(USERS_API_URL);
+	const usersData = await users.json();
+
+	return {
+		props: {
+			fallback: {
+				[USERS_API_URL]: usersData,
+			},
+		},
+	};
+};
+
+const Users = (props) => {
+	const { fallback } = props;
 	return (
 		<div>
 			<Head>
 				<title>Users Page</title>
 			</Head>
-			<Header />
-			<UsersComponent />
+			<SWRConfig value={{ fallback }}>
+				<Header />
+				<UsersComponent />
+			</SWRConfig>
 		</div>
 	);
 };
